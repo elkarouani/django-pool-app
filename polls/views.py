c8db8d465d4266446967219a524f6ac046b05632
KDragon <elkarouani@gmail.com>
2018-12-20 12:41:18 +0100

    Task: edit on templates (index and detail)

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.template import loader
 
 from .models import Question
@@ -17,11 +17,12 @@ def index(request):
 
 def detail(request, question_id):
 	# return HttpResponse("You're looking at question %s." % question_id)
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
+	# try:
+	# 	question = Question.objects.get(pk=question_id)
+	# except Question.DoesNotExist:
+	# 	raise Http404("Question does not exist")
 	
+	question = get_object_or_404(Question ,pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
