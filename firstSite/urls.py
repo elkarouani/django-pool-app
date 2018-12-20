f6c7e48b15d041b35470951857981087e3654188
KDragon <elkarouani@gmail.com>
2018-12-20 11:12:50 +0100

    Task: display latest five questions on index view

--- firstSite/urls.py
+++ firstSite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('polls', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -11,7 +12,6 @@ class Question(models.Model):
     
     def was_published_recently(self):
     	return self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
