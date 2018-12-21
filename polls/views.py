b4e7f03c0b77bfa1e6ab5adf19e8f2462f25d9c8
KDragon <elkarouani@gmail.com>
2018-12-21 08:13:47 +0100

    Task: setup our vote form

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 # from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -30,4 +31,17 @@ def results(request, question_id):
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	# return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question ,pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice."
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
