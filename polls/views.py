05cb27465bea695b2fa7648324d106131d4f76f2
KDragon <elkarouani@gmail.com>
2018-12-21 17:35:16 +0100

    Task: create result template

--- polls/views.py
+++ polls/views.py
@@ -27,8 +27,10 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question ,pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+	# response = "You're looking at the results of question %s."
+	# return HttpResponse(response % question_id)
 
 def vote(request, question_id):
 	# return HttpResponse("You're voting on question %s." % question_id)
