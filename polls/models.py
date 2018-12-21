4c57e5962fd78efced80efa4e64b64424ab5d2fd
KDragon <elkarouani@gmail.com>
2018-12-21 18:49:34 +0100

    Fix: was_published_recently methode

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
     	return self.question_text
     
     def was_published_recently(self):
-    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1) 
+    	return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now() 
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
