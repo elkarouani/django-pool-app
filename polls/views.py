0407029a00d4bd903d644f617851f591e1171e1a
elkarouani <elkarouani@gmail.com>
2024-04-10 01:40:24 +0000

    Task: add more views for voting and results

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Question
 
@@ -13,8 +15,20 @@ def detail(request, question_id):
   return render(request, "polls/detail.html", { "question": question })
 
 def results(request, question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, "polls/results.html", { "question": question })
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, "polls/detail.html", {
+      "question": question,
+      "error_message": "You didn't select a choice."
+    })
+  else:
+    selected_choice.votes = F("votes") + 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
